06fad7b89d25d9b7fe20bcebe8a4adcbc3f16fa4
Pierce Darragh <pierce.darragh@gmail.com>
2017-05-11 23:56:30 -0600

    Add lexer tests

---
+++ tests/test_lexer.py
@@ -0,0 +1,104 @@
+from viper.lexer import *
+
+import pytest
+
+from typing import Type
+
+
+###############################################################################
+#
+# INDIVIDUAL TOKENS
+#
+###############################################################################
+
+
+def _test_single_token(token: str, lexeme_type: Type):
+    lexemes = Lexer.lex_token(token)
+    assert len(lexemes) == 1
+    assert lexemes[0] == lexeme_type(token)
+
+
+def _test_bad_single_token(token: str, intended_type: Type):
+    try:
+        _test_single_token(token, intended_type)
+    except AssertionError:
+        assert True
+    except LexerError:
+        assert True
+
+
+# CORRECT TOKENS
+#
+# These tests should test all types of different tokens for each form of
+# lexeme.
+
+@pytest.mark.parametrize('token', [
+    '42',
+    '.42', '.42e8', '.42E8', '.42e+8', '.42E+8', '.42e-8', '.42E-8',
+    '42e8', '42E8', '42e+8', '42E+8', '42e-8', '42E-8',
+    '42.', '42.e8', '42.E8', '42.e+8', '42.E+8', '42.e-8', '42.E-8',
+    '4.2', '4.2e8', '4.2E8', '4.2e+8', '4.2E+8', '4.2e-8', '4.2E-8',
+])
+def test_number(token: str):
+    _test_single_token(token, Number)
+
+
+@pytest.mark.parametrize('token', [
+    '_', '__', '___',
+    'a', '_a', 'aa', 'aA', 'a1',
+    'a-b-c', 'a-B-C', 'a1-b2-c3',
+    'a!', 'a@', 'a$', 'a%', 'a^', 'a&', 'a*', 'a?',
+])
+def test_name(token: str):
+    _test_single_token(token, Name)
+
+
+@pytest.mark.parametrize('token', [
+    'A', 'AB', 'AThing', 'AnotherThing', 'A-Thing', 'Another-Thing', 'Plan9', 'Plan-9-From-Outer-Space',
+])
+def test_class(token: str):
+    _test_single_token(token, Class)
+
+
+@pytest.mark.parametrize('token', [
+    '!', '@', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', '|', ':', '/', '?', '<', '>',
+    '[', ']', '{', '}', '~',
+    '!@', '<>', '::',
+    '()', '(()', '()()', '())', '(())',
+])
+def test_operator(token: str):
+    _test_single_token(token, Operator)
+
+
+# INCORRECT TOKENS
+#
+# These tests should ensure that rogue tokens do not sneak through.
+
+@pytest.mark.parametrize('token', [
+    '42e', '42E', '42e+', '42E+', '42e-', '42E-',
+    '42.e', '42.E', '42.e+', '42.E+', '42.e-', '42.E-',
+    '4.2e', '4.2E', '4.2e+', '4.2E+', '4.2e-', '4.2E-',
+])
+def test_bad_number(token: str):
+    _test_bad_single_token(token, Number)
+
+
+@pytest.mark.parametrize('token', [
+    'A', 'AB', '-a', 'a-', 'a-b-', 'a-?', 'a?!', '42', '42e3', '?', '?!',
+])
+def test_bad_name(token: str):
+    _test_bad_single_token(token, Name)
+
+
+@pytest.mark.parametrize('token', [
+    'a-class', 'a_class', 'aA', '9', '9a', '!?',
+])
+def test_bad_class(token: str):
+    _test_bad_single_token(token, Class)
+
+
+@pytest.mark.parametrize('token', [
+    'a', 'A', 'aA', 'AA', 'Aa', 'aa', '(42)',
+])
+def test_bad_operator(token: str):
+    _test_bad_single_token(token, Operator)
