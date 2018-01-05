715098531f823c3b2932e6a03d2e4b113bd53ed9
Pierce Darragh <pierce.darragh@gmail.com>
2018-01-05 15:18:25 -0500

    Revise grammar tests for atom

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,25 +1,37 @@
 import viper.grammar as vg
-import viper.grammar.languages as vgl
 import viper.lexer as vl
+
+from viper.grammar.languages import (
+    SPPF,
+    ParseTreeEmpty as PTE, ParseTreeChar as PTC, ParseTreePair as PTP, ParseTreeRep as PTR
+)
 
 import pytest
 
 
 @pytest.mark.parametrize('line,sppf', [
     ('foo',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Name('foo')))),
-    ('2',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Number('2')))),
+     SPPF(PTC(vl.Name('foo')))),
+    ('42',
+     SPPF(PTC(vl.Number('42')))),
     ('...',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Operator('...')))),
+     SPPF(PTC(vl.Operator('...')))),
     ('Zilch',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Class('Zilch')))),
+     SPPF(PTC(vl.Class('Zilch')))),
     ('True',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Class('True')))),
+     SPPF(PTC(vl.Class('True')))),
     ('False',
-     vgl.SPPF(vgl.ParseTreeChar(vl.Class('False')))),
+     SPPF(PTC(vl.Class('False')))),
+    ('()',
+     SPPF(PTP(SPPF(PTC(vl.OpenParen())),
+              SPPF(PTC(vl.CloseParen()))))),
+    ('(foo)',
+     SPPF(PTP(SPPF(PTC(vl.OpenParen())),
+              SPPF(PTP(SPPF(PTP(SPPF(PTC(vl.Name('foo'))),
+                                SPPF(PTR(SPPF())))),
+                       SPPF(PTC(vl.CloseParen()))))))),
 ])
-def test_atom(line: str, sppf: vgl.SPPF):
+def test_atom(line: str, sppf: SPPF):
     atom = vg.GRAMMAR.get_rule('atom')
     lexemes = vl.lex_line(line)
     assert sppf == vg.make_sppf(atom, lexemes)
