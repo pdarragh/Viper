35b8dd77be54872bcf17b62e288cd4a2b5a37e5a
Pierce Darragh <pierce.darragh@gmail.com>
2018-03-10 10:53:06 -0700

    Add CLI option for outputting lexed files

--- viper.py
+++ viper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from viper.interactive import *
-from viper.lexer import lex_file
+from viper.lexer import lex_file, NewLine
 from viper.grammar import GRAMMAR
 
 
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-L', '--interactive-lexer', action='store_true', help='lexes input')
+    parser.add_argument('-l', '--file-lexer', help='produces lexemes for given file input')
     parser.add_argument('-S', '--interactive-sppf', action='store_true', help='lexes input and produces SPPF')
     parser.add_argument('-s', '--file-sppf', help='produces SPPF for given input')
     parser.add_argument('-r', '--grammar-rule', default='single_line', help='grammar rule from which to start parsing')
@@ -16,9 +17,20 @@ if __name__ == '__main__':
 
     if args.interactive_lexer:
         InteractiveLexer().cmdloop()
+    elif args.file_lexer:
+        lexemes = lex_file(args.file_lexer)
+        outputs = []
+        curr_line = []
+        for lexeme in lexemes:
+            if isinstance(lexeme, NewLine):
+                outputs.append(' '.join(map(repr, curr_line)))
+                curr_line = []
+            curr_line.append(lexeme)
+        outputs.append(' '.join(map(repr, curr_line)))
+        print('\n'.join(outputs))
     elif args.interactive_sppf:
         InteractiveSPPF(args.grammar_rule).cmdloop()
     elif args.file_sppf:
         lexemes = lex_file(args.file_sppf)
-        sppf = GRAMMAR.parse_multiple(lexemes)
+        sppf = GRAMMAR.parse_file(lexemes)
         print(sppf)
