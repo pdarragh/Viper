0f052bab7e969f674bdc1b9530bc353a243f665c
Pierce Darragh <pierce.darragh@gmail.com>
2018-03-24 14:04:56 -0600

    Add more dedicated grammar-parsing functions

--- viper/parser/grammar/parse_grammar.py
+++ viper/parser/grammar/parse_grammar.py
@@ -3,10 +3,10 @@ from .production_part import *
 from .tokenize.alt_token import *
 from .tokenize.grammar_tokenizer import tokenize_grammar_file
 
-from typing import Dict, List, NamedTuple
+from typing import Dict, List, NamedTuple, Type
 
 
-TokenParse = NamedTuple('TokenParse', [('part', ProductionPart), ('offset', int)])
+TokenParse = NamedTuple('TokenParse', [('part', ProductionPart), ('idx', int)])
 
 
 def parse_grammar_file(filename: str) -> Dict[str, List[ProductionPart]]:
@@ -54,12 +54,12 @@ def parse_production(token_list: List[AltToken]) -> NamedProduction:
     RuleAlias       = Rule
     Match           = '@'? MatchOpt
     MatchOpt        = Literal
-                    | DynamicMatch
-    DynamicMatch    = SpecialMatch
+                    | StaticMatch
+    StaticMatch     = SpecialMatch
                     | RuleMatch
     SpecialMatch    = Special ('*' | '?')?
     RuleMatch       = Rule ('*' | '?')?
-    Save            = ParamSpec ':' DynamicMatch
+    Save            = ParamSpec ':' StaticMatch
     ParamSpec       = ParamName
                     | '&' ParamName '{' ParamName ',' ParamName '}'
     ParamName       = LITERAL
@@ -76,43 +76,80 @@ def parse_production(token_list: List[AltToken]) -> NamedProduction:
     while i < len(token_list):
         token = token_list[i]
         if isinstance(token, LiteralToken):
-            parse = parse_literal_token(token_list, i)
+            parse = parse_literal(token_list, i)
         elif isinstance(token, SpecialToken):
-            parse = parse_special_token(token_list, i)
+            parse = parse_special_literal(token_list, i)
         elif isinstance(token, ParameterNameToken):
-            parse = parse_parameter_name_token(token_list, i)
+            parse = parse_regular_parameter(token_list, i)
         elif isinstance(token, ParameterExpansionToken):
-            parse = parse_parameter_expansion_token(token_list, i)
+            parse = parse_expanded_parameter(token_list, i)
         elif isinstance(token, SpecialParameterExpansionToken):
-            parse = parse_special_parameter_expansion_token(token_list, i)
+            parse = parse_special_expanded_parameter(token_list, i)
         else:
             # TODO: Make custom error here.
             raise RuntimeError
         parts.append(parse.part)
-        i += parse.offset
+        i = parse.idx
     return NamedProduction(name, parts)
 
 
-def parse_literal_token(token_list: List[AltToken], index: int) -> TokenParse:
+def parse_literal(token_list: List[AltToken], index: int) -> TokenParse:
     part = LiteralPart(token_list[index].text)
-    return TokenParse(part, 1)
+    return TokenParse(part, index + 1)
 
 
-def parse_special_token(token_list: List[AltToken], index: int) -> TokenParse:
+def parse_special_literal(token_list: List[AltToken], index: int) -> TokenParse:
     return parse_special_token_or_rule_token(token_list, index, True)
 
 
-def parse_parameter_name_token(token_list: List[AltToken], index: int) -> TokenParse:
+def parse_rule_match(token_list: List[AltToken], index: int) -> TokenParse:
+    return parse_special_token_or_rule_token(token_list, index, False)
+
+
+def parse_regular_parameter(token_list: List[AltToken], index: int) -> TokenParse:
+    parameter_name = token_list[index].text
+    if not isinstance(token_list[index + 1], ColonToken):
+        # TODO: Make custom error here.
+        raise RuntimeError
+    index += 2
+    match_token = token_list[index]
+    if isinstance(match_token, SpecialToken):
+        match_parse = parse_special(token_list, index)
+    elif isinstance(match_token, RuleToken):
+        match_parse = parse_rule_match(token_list, index)
+    else:
+        # TODO: Make custom error here.
+        raise RuntimeError
+    match_parse = parse_possible_repeatable_or_optional(token_list, match_parse.idx, match_parse.part)
+    return TokenParse(ParameterPart(parameter_name, match_parse.part), match_parse.idx)
+
+
+def parse_expanded_parameter(token_list: List[AltToken], index: int) -> TokenParse:
     pass
 
 
-def parse_parameter_expansion_token(token_list: List[AltToken], index: int) -> TokenParse:
-    pass
-
-
-def parse_special_parameter_expansion_token(token_list: List[AltToken], index: int) -> TokenParse:
+def parse_special_expanded_parameter(token_list: List[AltToken], index: int) -> TokenParse:
     pass
 
 
 def parse_special_token_or_rule_token(token_list: List[AltToken], index: int, is_special: bool) -> TokenParse:
-    pass
+    if is_special:
+        parse = parse_special(token_list, index)
+    else:
+        parse = parse_literal(token_list, index)
+    return parse_possible_repeatable_or_optional(token_list, parse.idx, parse.part)
+
+
+def parse_special(token_list: List[AltToken], index: int) -> TokenParse:
+    part = SpecialPart(token_list[index].text)
+    return TokenParse(part, index + 1)
+
+
+def parse_possible_repeatable_or_optional(token_list: List[AltToken], index: int, enclosed_part: ProductionPart) -> TokenParse:
+    token = token_list[index]
+    if isinstance(token, RepeatToken):
+        return TokenParse(RepeatPart(enclosed_part), index + 1)
+    elif isinstance(token, OptionalToken):
+        return TokenParse(OptionPart(enclosed_part), index + 1)
+    else:
+        return TokenParse(enclosed_part, index)
--- viper/parser/grammar/production_part.py
+++ viper/parser/grammar/production_part.py
@@ -5,3 +5,24 @@ class ProductionPart:
 class LiteralPart(ProductionPart):
     def __init__(self, text: str):
         self.text = text
+
+
+class SpecialPart(ProductionPart):
+    def __init__(self, text: str):
+        self.text = text
+
+
+class RepeatPart(ProductionPart):
+    def __init__(self, repeated_part: ProductionPart):
+        self.part = repeated_part
+
+
+class OptionPart(ProductionPart):
+    def __init__(self, optional_part: ProductionPart):
+        self.part = optional_part
+
+
+class ParameterPart(ProductionPart):
+    def __init__(self, name: str, matched_part: ProductionPart):
+        self.name = name
+        self.part = matched_part
