792b53636c40822e3ff96e092249bce976bb66f1
Pierce Darragh <pierce.darragh@gmail.com>
2018-03-24 15:16:23 -0600

    Add repr overrides for Productions and ProductionParts

--- viper/parser/grammar/production.py
+++ viper/parser/grammar/production.py
@@ -4,15 +4,22 @@ from typing import List
 
 
 class Production:
-    pass
+    def __str__(self):
+        return repr(self)
 
 
 class RuleAliasProduction(Production):
     def __init__(self, rule_name: str):
         self.name = rule_name
 
+    def __repr__(self):
+        return "<" + self.name + ">"
+
 
 class NamedProduction(Production):
     def __init__(self, production_name: str, production_parts: List[ProductionPart]):
         self.name = production_name
         self.parts = production_parts
+
+    def __repr__(self):
+        return self.name + " = " + ' '.join(map(repr, self.parts))
--- viper/parser/grammar/production_part.py
+++ viper/parser/grammar/production_part.py
@@ -1,30 +1,46 @@
 class ProductionPart:
-    pass
+    def __str__(self):
+        return repr(self)
 
 
 class LiteralPart(ProductionPart):
     def __init__(self, text: str):
         self.text = text
 
+    def __repr__(self):
+        return "'" + self.text + "'"
+
 
 class SpecialPart(ProductionPart):
     def __init__(self, special_token: str):
         self.token = special_token
+
+    def __repr__(self):
+        return self.token
 
 
 class RulePart(ProductionPart):
     def __init__(self, rule_name: str):
         self.name = rule_name
 
+    def __repr__(self):
+        return "<" + self.name + ">"
+
 
 class RepeatPart(ProductionPart):
     def __init__(self, repeated_part: ProductionPart):
         self.part = repeated_part
 
+    def __repr__(self):
+        return repr(self.part) + "*"
+
 
 class OptionPart(ProductionPart):
     def __init__(self, optional_part: ProductionPart):
         self.part = optional_part
+
+    def __repr__(self):
+        return repr(self.part) + "?"
 
 
 class ParameterPart(ProductionPart):
@@ -32,10 +48,16 @@ class ParameterPart(ProductionPart):
         self.name = name
         self.part = matched_part
 
+    def __repr__(self):
+        return self.name + ": " + repr(self.part)
+
 
 class ExpandedParameterPart(ProductionPart):
     def __init__(self, rule_part: ProductionPart):
         self.rule = rule_part
+
+    def __repr__(self):
+        return "@" + repr(self.rule)
 
 
 class SpecialExpandedParameterPart(ProductionPart):
@@ -44,3 +66,6 @@ class SpecialExpandedParameterPart(ProductionPart):
         self.single = single_param
         self.list = list_param
         self.rule = rule_part
+
+    def __repr__(self):
+        return "&" + self.name + "{" + self.single + ", " + self.list + "}: " + repr(self.rule)
