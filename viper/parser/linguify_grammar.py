5847a2868886e8eb247c6037bad37b8f6f9e9e1e
Pierce Darragh <pierce.darragh@gmail.com>
2018-03-30 11:54:15 -0600

    Implement basic ASTNode construction through reductions

--- viper/parser/linguify_grammar.py
+++ viper/parser/linguify_grammar.py
@@ -1,3 +1,4 @@
+from .ast import ASTNode
 from .grammar_parsing.parse_grammar import parse_grammar_file
 from .grammar_parsing.production import *
 from .grammar_parsing.production_part import *
@@ -7,10 +8,12 @@ from .languages import *
 from viper.error import ViperError
 from viper.lexer import Lexeme
 
-from typing import Dict, List
+from typing import Any, Callable, Dict, List, NamedTuple, Optional
 
 
 RuleDict = Dict[str, Language]
+ParamFunc = Callable[[SPPF], Any]
+PartTuple = NamedTuple('PartTuple', [('lang', Language), ('name', Optional[str])])
 
 
 class LinguifierError(ViperError):
@@ -48,41 +51,107 @@ def linguify_rule_alias_production(production: RuleAliasProduction, rule_dict: R
 
 
 def linguify_named_production(production: NamedProduction, rule_dict: RuleDict) -> Language:
-    # TODO: make AST object
-    # TODO: create language from production parts
-    ...
+    param_names: List[Optional[str]] = []
     part_langs = []
     for part in production.parts:
-        lang = linguify_production_part(part, rule_dict)
-        part_langs.append(lang)
+        part = linguify_production_part(part, rule_dict)
+        part_langs.append(part.lang)
+        param_names.append(part.name)
     prod_lang = concat(*part_langs)
-    ...
+    prod_lang = make_ast_red(prod_lang, production.name, param_names)
     return prod_lang
+
+
+class ASTNodeRedFunc(RedFunc):
+    def __init__(self, name: str, param_names: List[Optional[str]]):
+        self.name = name
+        self.param_names = param_names
+
+    def make_nice_string(self, start_column: int) -> str:
+        return self.name
+
+    def __call__(self, sppf: SPPF) -> SPPF:
+        """
+        The SPPF for a production will look something like:
+
+            (pair (char ...)
+                  (pair (char ...)
+                        (pair (pair (...
+                                     ...)
+                              (pair (char ...)
+                                    (char ...)))))
+
+        :param sppf: the SPPF for the production we expect to produce (already parse_null'ed)
+        :return: a simplified SPPF
+        """
+        params = {}
+        curr = sppf
+        for name in self.param_names:
+            if not curr:
+                # There was nothing for the parameter to match.
+                return SPPF()
+            if len(curr) > 1:
+                # TODO: Not sure if this is correct.
+                raise LinguifierError(f"SPPF has too many children: {curr}")
+            child = curr[0]
+            if isinstance(child, ParseTreeEmpty):
+                return SPPF()
+            elif isinstance(child, ParseTreeEps):
+                if name is None:
+                    curr = SPPF()
+                else:
+                    params[name] = None
+            elif isinstance(child, ParseTreeChar):
+                if name is not None:
+                    params[name] = child.token
+                curr = SPPF()
+            elif isinstance(child, ParseTreePair):
+                param_sppf = child.left
+                if len(param_sppf) != 1:
+                    raise LinguifierError(f"Invalid child SPPF: {param_sppf}")
+                if name is not None:
+                    params[name] = param_sppf[0]
+                curr = child.right
+            else:
+                raise LinguifierError("Invalid child node.")
+        return SPPF(ParseTreeChar(ASTNode(self.name, params)))
+
+
+def make_ast_red(lang: Language, name: str, param_names: List[Optional[str]]) -> Language:
+    func = ASTNodeRedFunc(name, param_names)
+    return red(lang, func)
 
 
 def make_rule_literal(rule_name: str, rule_dict: RuleDict) -> RuleLiteral:
     return RuleLiteral(rule_name, rule_dict)
 
 
-def linguify_production_part(part: ProductionPart, rule_dict: RuleDict) -> Language:
+def linguify_production_part(part: ProductionPart, rule_dict: RuleDict) -> PartTuple:
     if isinstance(part, LiteralPart):
-        return literal(Lexeme(part.text))
+        part = literal(Lexeme(part.text))
+        return PartTuple(part, None)
     elif isinstance(part, SpecialPart):
-        return literal(SPECIAL_TOKENS[part.token])
+        part = literal(SPECIAL_TOKENS[part.token])
+        return PartTuple(part, None)
     elif isinstance(part, RulePart):
-        return make_rule_literal(part.name, rule_dict)
+        part = make_rule_literal(part.name, rule_dict)
+        return PartTuple(part, None)
     elif isinstance(part, RepeatPart):
-        return rep(linguify_production_part(part.part, rule_dict))
+        inner_part = linguify_production_part(part.part, rule_dict)
+        part = rep(inner_part.lang)  # TODO: Ensure inner_part.func is unneeded.
+        return PartTuple(part, None)
     elif isinstance(part, OptionPart):
-        return opt(linguify_production_part(part.part, rule_dict))
+        inner_part = linguify_production_part(part.part, rule_dict)
+        part = opt(inner_part.lang)  # TODO: Ensure inner_part.func is unneeded.
+        return PartTuple(part, None)
     elif isinstance(part, ParameterPart):
-        # TODO: Fix this.
-        return linguify_production_part(part.part, rule_dict)
+        inner_part = linguify_production_part(part.part, rule_dict)
+        return PartTuple(inner_part.lang, part.name)
     elif isinstance(part, ExpandedParameterPart):
         # TODO: Fix this.
         return linguify_production_part(part.rule, rule_dict)
     elif isinstance(part, SpecialExpandedParameterPart):
-        # TODO: Fix this.
+        # TODO: Incorporate sep_rep. sep_lang is a language of the separator.
         return linguify_production_part(part.rule, rule_dict)
     else:
         raise LinguifierError(f"Cannot linguify unknown production part: {part}")
