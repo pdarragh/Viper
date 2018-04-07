7777798169a1ad58abadbca0fa6794a4cdcce778
Pierce Darragh <pierce.darragh@gmail.com>
2018-04-06 23:17:15 -0600

    Rewrite node generator to use a tree for organizing classes

--- viper/parser/ast/generate_nodes_module.py
+++ viper/parser/ast/generate_nodes_module.py
@@ -6,93 +6,126 @@ from typing import Dict, List, Optional, Tuple
 
 
 # TODO: Address invariant: parameter lifting can only be applied to monoproduction rules.
+    
+    
+# Three levels:
+#
+#   1. Rules that are superclasses of other rules (through aliasing).
+#   2. Rules that are superclasses only of productions.
+#   3. Productions.
+#
+# Classes in (1) must be written first, then (2), then (3).
 
 
-def generate_from_grammar_file(grammar_filename: str, output_filename: str):
-    ASTNodeGenerator(grammar_filename).generate(output_filename)
+Arg = Tuple[str, Optional[str]]
+ArgList = List[Arg]
 
 
 class ASTNodeGenerator:
     BASE_AST_CLASS_NAME = 'AST'
 
     def __init__(self, filename: str):
-        self.lines: List[str] = []
-        self.class_names: Dict[str, str] = {}
-        self.rule_superclasses: Dict[str, str] = {}
-        self.production_superclasses: Dict[str, str] = {}
-        self.initialize()
+        self.tree = None
         parsed_rules = parse_grammar_file(filename)
-        self.pre_process_rules(parsed_rules)
+        self.build_rule_tree(parsed_rules)
         for rule, production_list in parsed_rules.items():
             if len(production_list) == 1:
                 self.make_ast_node_class_from_single_production(rule, production_list[0])
             else:
                 self.make_ast_node_classes_from_production_list(rule, production_list)
 
-    def initialize(self):
-        self.add_line("# This file was automatically generated.")
-        self.add_line("")
-        self.add_line("")
-        self.add_line("class AST:")
-        self.add_line("    pass")
-        self.add_line("")
-        self.add_line("")
+    class ClassTree:
+        def __init__(self):
+            self._nodes = {}
+            root = ASTNodeGenerator.ClassTreeNode('AST')
+            root.lines = [
+                'class AST:',
+                '    pass'
+            ]
+            self._nodes['AST'] = root
+            self.root = root
 
-    def generate(self, output_filename: str):
-        lines = '\n'.join(self.lines)
-        lines = lines.format(**self.class_names)
-        with open(output_filename, 'w') as of:
-            of.write(lines)
+        def __getitem__(self, item):
+            return self._nodes[item]
 
-    def pre_process_rules(self, parsed_rules: Dict[str, List[Production]]):
+        def __setitem__(self, key, value):
+            if key in self._nodes:
+                raise RuntimeError  # TODO: Replace with custom error.
+            self._nodes[key] = value
+
+    class ClassTreeNode:
+        def __init__(self, name: str):
+            self.name = name
+            self.parents = []
+            self.children = []
+            self.lines = []
+
+        def add_new_child(self, child_name: str):
+            node = ASTNodeGenerator.ClassTreeNode(child_name)
+            node.parents.append(self)
+            self.children.append(node)
+            return node
+
+    def build_rule_tree(self, parsed_rules: Dict[str, List[Production]]):
+        self.tree = ASTNodeGenerator.ClassTree()
+
+        rules = set()       # All rules.
+        aliases = {}        # Map: aliased-rule -> [parent rules]
+        productions = {}    # Map: prod-name -> parent rule
+
         for rule, production_list in parsed_rules.items():
-            rule_class_name = self.convert_rule_name_to_class(rule)
-            self.class_names[rule] = rule_class_name
-            if len(production_list) == 1:
-                continue
+            rules.add(rule)
             for production in production_list:
-                self.pre_process_production(rule, production)
+                if isinstance(production, RuleAliasProduction):
+                    parents = aliases.get(production.name, [])
+                    parents.append(rule)
+                    aliases[production.name] = parents
+                elif isinstance(production, NamedProduction):
+                    productions[production.name] = rule
+                else:
+                    raise RuntimeError
 
-    def pre_process_production(self, rule: str, production: Production):
-        if isinstance(production, RuleAliasProduction):
-            self.rule_superclasses[production.name] = rule
-        elif isinstance(production, NamedProduction):
-            self.production_superclasses[production.name] = rule
-        else:
-            raise RuntimeError  # TODO: Replace with custom error.
+        for rule in rules:
+            self.tree[rule] = self.tree.root.add_new_child(rule)
+        for alias, parents in aliases.items():
+            node = self.tree[alias]
+            for parent in parents:
+                parent_node = self.tree[parent]
+                node.parents.append(parent_node)
+                parent_node.children.append(node)
+        for production, parent in productions.items():
+            parent_node = self.tree[parent]
+            self.tree[production] = parent_node.add_new_child(production)
 
     def make_ast_node_class_from_single_production(self, rule: str, production: Production):
-        class_name = self.convert_rule_name_to_class(rule)
         if isinstance(production, RuleAliasProduction):
             # This was handled in pre-processing.
             return
         elif isinstance(production, NamedProduction):
             args = self.get_args_from_production(production)
-            self.make_ast_node_class(class_name, self.BASE_AST_CLASS_NAME, args)
+            self.make_ast_node_class(rule, args)
         else:
             raise RuntimeError  # TODO: Replace with custom error.
 
     def make_ast_node_classes_from_production_list(self, rule: str, production_list: List[Production]):
         # Make the base class for these productions to inherit from.
-        superclass_name = self.convert_rule_name_to_class(rule)
-        superclass_base_class = self.rule_superclasses.get(rule, self.BASE_AST_CLASS_NAME)
-        self.make_ast_node_class(superclass_name, superclass_base_class, [])
+        self.make_ast_node_class(rule, [])
         # Now create each child class.
         for production in production_list:
             if isinstance(production, NamedProduction):
                 class_name = production.name
                 args = self.get_args_from_production(production)
-                self.make_ast_node_class(class_name, superclass_name, args)
+                self.make_ast_node_class(class_name, args)
 
-    def get_args_from_production(self, production: NamedProduction) -> List[Tuple[str, str]]:
-        args: List[Tuple[str, str]] = []
+    def get_args_from_production(self, production: NamedProduction) -> List[Arg]:
+        args: List[Arg] = []
         for part in production.parts:
             if isinstance(part, ParameterPart):
                 if isinstance(part.part, RulePart):
-                    type = part.part.name
+                    arg_type = part.part.name
                 else:
-                    type = None
-                pair = (part.name, type)
+                    arg_type = None
+                pair = (part.name, arg_type)
                 args.append(pair)
             elif isinstance(part, LiftedParameterPart):
                 ...
@@ -100,24 +133,24 @@ class ASTNodeGenerator:
                 continue
         return args
 
-    def make_ast_node_class(self, class_name: str, superclass: Optional[str], args: List[Tuple[str, str]]):
-        if superclass is None:
-            inheritance = ''
-        else:
-            inheritance = '(' + superclass + ')'
-        self.add_line(f"class {class_name}{inheritance}:")
+    def make_ast_node_class(self, class_rule_name: str, args: List[Arg]):
+        node = self.tree[class_rule_name]
+        lines = node.lines
+        class_name = self.convert_rule_name_to_class_name(class_rule_name)
+        superclasses = ', '.join([self.convert_rule_name_to_class_name(parent.name) for parent in node.parents])
+
+        def param_from_arg(arg: Arg) -> str:
+            arg_name, arg_type = arg
+            return arg_name if arg_type is None else arg_name + ": {" + arg_type + "}"
+
+        params = ', '.join(map(param_from_arg, [('self', None)] + args))
+        lines.append(f"class {class_name}({superclasses}):")
         if args:
-            params = ', '.join(name + (": {" + type + "}" if type is not None else "") for name, type in args)
-            self.add_line(f"    def __init__(self, {params}):")
+            lines.append(f"    def__init__({params}):")
             for name, _ in args:
-                self.add_line(f"        self.{name} = {name}")
+                lines.append(f"        self.{name} = {name}")
         else:
-            self.add_line("    pass")
-        self.add_line("")
-        self.add_line("")
+            lines.append(f"    pass")
 
-    def convert_rule_name_to_class(self, rule: str) -> str:
+    def convert_rule_name_to_class_name(self, rule: str) -> str:
         return ''.join(map(lambda s: s.title(), rule.split('_')))
-
-    def add_line(self, line: str):
-        self.lines.append(line)
