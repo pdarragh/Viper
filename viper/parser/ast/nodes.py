b65899068c5dc8145811bcb39a59e201ed07b208
Pierce Darragh <pierce.darragh@gmail.com>
2018-04-15 13:18:40 -0600

    Update AST node classes

--- viper/parser/ast/nodes.py
+++ viper/parser/ast/nodes.py
@@ -1,5 +1,7 @@
 # This module was automatically generated.
 
+import viper.lexer as vl
+
 from typing import List, Optional
 
 
@@ -7,11 +9,11 @@ class AST:
     pass
 
 
-class ExprStmt(AST):
+class SingleInput(AST):
     pass
 
 
-class Trailer(AST):
+class FileLines(AST):
     pass
 
 
@@ -19,22 +21,11 @@ class Suite(AST):
     pass
 
 
-class SingleInput(AST):
-    pass
-
-
 class Stmt(AST):
     pass
 
 
-class Parameter(AST):
-    def __init__(self, external: str, internal: str, param_type: str):
-        self.external = external
-        self.internal = internal
-        self.param_type = param_type
-
-
-class FileLines(AST):
+class ExprStmt(AST):
     pass
 
 
@@ -42,21 +33,28 @@ class Atom(AST):
     pass
 
 
+class Trailer(AST):
+    pass
+
+
+class Parameter(AST):
+    def __init__(self, external: vl.Name, internal: vl.Name, param_type: vl.Class):
+        self.external = external
+        self.internal = internal
+        self.param_type = param_type
+
+
+class FileInput(AST):
+    def __init__(self, lines: List[FileLines]):
+        self.lines = lines
+
+
 class SimpleStmt(Stmt):
     def __init__(self, expr_stmt: ExprStmt):
         self.expr_stmt = expr_stmt
 
 
-class Name(Atom):
-    def __init__(self, name: str):
-        self.name = name
-
-
-class PassStmt(ExprStmt):
-    pass
-
-
-class FileNewline(FileLines):
+class CompoundStmt(Stmt):
     pass
 
 
@@ -66,7 +64,11 @@ class Expr(AST):
         self.trailers = trailers
 
 
-class Ellipsis(Atom):
+class SingleNewline(SingleInput):
+    pass
+
+
+class FileNewline(FileLines):
     pass
 
 
@@ -80,34 +82,57 @@ class ComplexSuite(Suite):
         self.stmts = stmts
 
 
-class FileInput(AST):
-    def __init__(self, lines: List[FileLines]):
-        self.lines = lines
-
-
-class CompoundStmt(Stmt):
+class PassStmt(ExprStmt):
     pass
+
+
+class Name(Atom):
+    def __init__(self, name: vl.Name):
+        self.name = name
 
 
 class Number(Atom):
-    def __init__(self, num: str):
+    def __init__(self, num: vl.Number):
         self.num = num
 
 
-class Field(Trailer):
-    def __init__(self, field: str):
-        self.field = field
-
-
-class SingleNewline(SingleInput):
+class Ellipsis(Atom):
     pass
 
 
+class Field(Trailer):
+    def __init__(self, field: vl.Name):
+        self.field = field
+
+
 class SubOpExpr(AST):
-    def __init__(self, first_op: str, ops: str, expr: Expr):
+    def __init__(self, first_op: vl.Operator, ops: vl.Operator, expr: Expr):
         self.first_op = first_op
         self.ops = ops
         self.expr = expr
+
+
+class ExprList(AST):
+    def __init__(self, exprs: List[Expr]):
+        self.exprs = exprs
+
+
+class FuncDef(CompoundStmt):
+    def __init__(self, name: vl.Name, params: List[Parameter], func_type: vl.Class, body: Suite):
+        self.name = name
+        self.params = params
+        self.func_type = func_type
+        self.body = body
+
+
+class Arguments(AST):
+    def __init__(self, args: List[Expr]):
+        self.args = args
+
+
+class SimpleLine(SingleInput):
+    def __init__(self, line: SimpleStmt):
+        self.line = line
 
 
 class ComplexLine(SingleInput):
@@ -120,53 +145,30 @@ class SimpleSuite(Suite):
         self.stmt = stmt
 
 
-class FuncDef(CompoundStmt):
-    def __init__(self, name: str, params: List[Parameter], func_type: str, body: Suite):
-        self.name = name
-        self.params = params
-        self.func_type = func_type
-        self.body = body
-
-
-class SimpleLine(SingleInput):
-    def __init__(self, line: SimpleStmt):
-        self.line = line
-
-
-class ExprList(AST):
-    def __init__(self, exprs: List[Expr]):
-        self.exprs = exprs
-
-
-class Arguments(AST):
-    def __init__(self, args: List[Expr]):
-        self.args = args
-
-
 class OpExpr(AST):
-    def __init__(self, left_ops: str, expr: Expr, sub_op_exprs: List[SubOpExpr], right_ops: str):
+    def __init__(self, left_ops: vl.Operator, expr: Expr, sub_op_exprs: List[SubOpExpr], right_ops: vl.Operator):
         self.left_ops = left_ops
         self.expr = expr
         self.sub_op_exprs = sub_op_exprs
         self.right_ops = right_ops
 
 
-class DataDef(CompoundStmt):
-    def __init__(self, name: str, args: Optional[Arguments], body: Suite):
+class ClassDef(CompoundStmt):
+    def __init__(self, name: vl.Class, args: Optional[Arguments], body: Suite):
         self.name = name
         self.args = args
         self.body = body
 
 
 class InterfaceDef(CompoundStmt):
-    def __init__(self, name: str, args: Optional[Arguments], body: Suite):
+    def __init__(self, name: vl.Class, args: Optional[Arguments], body: Suite):
         self.name = name
         self.args = args
         self.body = body
 
 
-class ClassDef(CompoundStmt):
-    def __init__(self, name: str, args: Optional[Arguments], body: Suite):
+class DataDef(CompoundStmt):
+    def __init__(self, name: vl.Class, args: Optional[Arguments], body: Suite):
         self.name = name
         self.args = args
         self.body = body
