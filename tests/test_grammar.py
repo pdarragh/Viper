beea5f33f3acc5929daedad7afb964b03629a257
Pierce Darragh <pierce.darragh@gmail.com>
2018-04-15 16:18:21 -0600

    Add Expr tests

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -22,3 +22,33 @@ import pytest
 def test_atom(line: str, ast: AST):
     lexemes = lex_line(line)
     assert ast == GRAMMAR.parse_rule('atom', lexemes)
+
+
+@pytest.mark.parametrize('line,ast', [
+    ('foo',
+     Expr(Name(vl.Name('foo')), [])),
+    ('foo.bar',
+     Expr(Name(vl.Name('foo')), [Field(vl.Name('bar'))])),
+    ('foo.bar.baz',
+     Expr(Name(vl.Name('foo')), [Field(vl.Name('bar')), Field(vl.Name('baz'))])),
+    ('foo.bar(baz)',
+     Expr(Name(vl.Name('foo')), [Field(vl.Name('bar')), Call([Expr(Name(vl.Name('baz')), [])])])),
+    ('foo.bar(baz, qux)',
+     Expr(Name(vl.Name('foo')), [
+         Field(vl.Name('bar')),
+         Call([Expr(Name(vl.Name('baz')), []), Expr(Name(vl.Name('qux')), [])])
+     ])),
+    ('foo.bar(baz, qux.quum())',
+     Expr(Name(vl.Name('foo')), [
+         Field(vl.Name('bar')),
+         Call([
+             Expr(Name(vl.Name('baz')), []),
+             Expr(Name(vl.Name('qux')), [Field(vl.Name('quum')), Call(None)])
+         ])
+     ])),
+    ('2.foo',
+     Expr(Number(vl.Number('2')), [Field(vl.Name('foo'))])),
+])
+def test_expr(line: str, ast: AST):
+    lexemes = lex_line(line)
+    assert ast == GRAMMAR.parse_rule('expr', lexemes)
