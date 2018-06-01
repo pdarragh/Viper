371984ced26a6feb9e94570d3f497db1bb33b7a0
Pierce Darragh <pierce.darragh@gmail.com>
2018-06-01 00:27:25 -0600

    Update lexeme tests for new Equals token

--- tests/lexeme_files/simple.py
+++ tests/lexeme_files/simple.py
@@ -1,6 +1,6 @@
 from viper.lexer import (
     NEWLINE as NL, INDENT as IND, DEDENT as DED, COLON as COL, COMMA as COM, OPEN_PAREN as OP, CLOSE_PAREN as CP,
-    ENDMARKER as EM, R_ARROW as ARR,
+    ENDMARKER as EM, R_ARROW as ARR, EQUALS as EQ,
     Number as Num,
     Name as N, ReservedName as RN,
     Class as C,
@@ -10,7 +10,7 @@ from viper.lexer import (
 lexemes = [
     RN('def'), N('simple'), OP, N('foo'), COL, C('A'), COM, N('bar'), COL, C('B'), CP, ARR, C('C'), COL,
     NL, IND,
-        N('foo'), Op('='), N('bar'), Op('+'), Num('42'), NL,
+        N('foo'), EQ, N('bar'), Op('+'), Num('42'), NL,
         N('bar'), Op('*='), Num('17'), NL,
         RN('return'), N('foo'), Op('>=>'), N('bar'), NL,
     DED,
